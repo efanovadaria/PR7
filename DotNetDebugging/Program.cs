002e82465568ef00bdfa850393e00274ef8e3fc8
Efanova Daria <efanova7335@mail.ru>
2026-05-07 12:02:01 +0300

    добавила XML-комментарии

--- DotNetDebugging/Program.cs
+++ DotNetDebugging/Program.cs
@@ -1,9 +1,23 @@
-﻿int result = Fibonacci(5);
+﻿/// <summary>
+/// Точка входа в программу.
+/// Вычисляет число Фибоначчи и выводит результат на экран.
+/// </summary>
+int result = Fibonacci(5);
 Console.WriteLine(result);
 
+/// <summary>
+/// Вычисляет число Фибоначчи по указанному номеру.
+/// </summary>
+/// <param name="n">
+/// Позиция числа в последовательности Фибоначчи.
+/// </param>
+/// <returns>
+/// Возвращает число Фибоначчи для указанного значения <paramref name="n"/>.
+/// </returns>
 static int Fibonacci(int n)
 {
     Console.WriteLine("The output is: ");
+
     int n1 = 0;
     int n2 = 1;
     int sum;
